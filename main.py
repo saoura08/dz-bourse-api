88d2387b7393551d54f3e1e2ca7660210aeac899
saoura08 <mohamedrakik004@gmail.com>
2025-06-13 20:16:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from typing import List, Dict
 
 app = FastAPI(title="DZ Bourse API")
 
+@app.get("/")
+def root():
+    return {"message": "مرحبا بك في DZ Bourse API! استعمل /api/stocks لعرض بيانات الأسهم."}
+
 @app.get("/api/stocks", response_model=List[Dict[str, str]])
 def read_stocks():
     return get_stock_data()
